77aebfec8c655a64af1ad8cdfea803a13fca1913
Todd Fisher <taf2@glazgo.(none)>
2009-04-30 10:12:03 -0400

    increase test duration

--- ext/test.rb
+++ ext/test.rb
@@ -2,3 +2,13 @@ require 'rmem'
 
 bytes = RMem::Report.memory
 puts format("resources: %d bytes, %.2f kbytes, and %.4f MB", bytes, bytes/1024.0, bytes/1024.0/1024.0)
+
+t = Time.now
+N = 10000
+N.times do
+  bytes = RMem::Report.memory
+  #puts format("resources: %d bytes, %.2f kbytes, and %.4f MB", bytes, bytes/1024.0, bytes/1024.0/1024.0)
+end
+puts "#{N} Iterations took: #{Time.now - t} seconds"
+bytes = RMem::Report.memory
+puts format("resources: %d bytes, %.2f kbytes, and %.4f MB", bytes, bytes/1024.0, bytes/1024.0/1024.0)
