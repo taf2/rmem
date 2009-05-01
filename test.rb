85290ce5eb35efbb9d25d772cde6702ae76e4b71
Todd Fisher <todd.fisher@gmail.com>
2009-04-30 20:05:59 -0400

    update test case to report memory usage 3 times

--- test.rb
+++ test.rb
@@ -1,13 +1,18 @@
 $:.unshift File.join(File.dirname(__FILE__), 'ext' )
 require 'rmem'
+t = Time.now
 
 bytes = RMem::Report.memory
 puts format("resources: %d bytes, %.2f kbytes, and %.4f MB", bytes, bytes/1024.0, bytes/1024.0/1024.0)
-t = Time.now
 N = 10000
 N.times do
   bytes = RMem::Report.memory
 end
-puts "#{N} Iterations took: #{Time.now - t} seconds"
 bytes = RMem::Report.memory
 puts format("resources: %d bytes, %.2f kbytes, and %.4f MB", bytes, bytes/1024.0, bytes/1024.0/1024.0)
+N.times do
+  bytes = RMem::Report.memory
+end
+bytes = RMem::Report.memory
+puts format("resources: %d bytes, %.2f kbytes, and %.4f MB", bytes, bytes/1024.0, bytes/1024.0/1024.0)
+puts "#{2*N + 2} Iterations took: #{Time.now - t} seconds"
